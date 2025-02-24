85994253d11d72ec58d94e9dcd6bf943d492f7d0
BrooksConway <brooksconway@unomaha.edu>
2025-02-24 15:56:38 +0000

    In progress

--- DiceRoll.py
+++ DiceRoll.py
@@ -4,13 +4,22 @@
 #Assignment:
 import random
 
+start = 1
+end = 6
+
 def main():
   #Create an empty list with possible roll values
   rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
   #Create two dice values ranging from 1 - 6 each
-  
+D1 = random.randint(start, end)
+print(D1)
+
+D2 = random.randint(start, end)
+print(D2)
+
   #find the sum total of the two dice
-  
+sumD = D1 + D2
+print(sumD)
   #print statictics for dice rolls
 
 
